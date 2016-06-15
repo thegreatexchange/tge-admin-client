2c45deef71868abed58c1a92e05acafab4555542
briandavidwetzel <briandavidwetzel@gmail.com>
2016-06-14 23:02:51 -0400

    WIP - UX for contacts

--- app/pods/contacts/controller.js
+++ app/pods/contacts/controller.js
@@ -9,26 +9,26 @@ export default Ember.Controller.extend({
   filterText:      null,
 
   contacts: [
-    { id: '1',  email: 'contact1@example.com',  name: 'Contact One' },
-    { id: '2',  email: 'contact2@example.com',  name: 'Contact Two' },
-    { id: '3',  email: 'contact3@example.com',  name: 'Contact Three' },
-    { id: '4',  email: 'contact4@example.com',  name: 'Contact Four' },
-    { id: '5',  email: 'contact5@example.com',  name: 'Contact Five' },
-    { id: '6',  email: 'contact6@example.com',  name: 'Contact Six' },
-    { id: '7',  email: 'contact7@example.com',  name: 'Contact Seven' },
-    { id: '8',  email: 'contact8@example.com',  name: 'Contact Eight' },
-    { id: '9',  email: 'contact9@example.com',  name: 'Contact Nine' },
-    { id: '10', email: 'contact10@example.com', name: 'Contact Ten' },
-    { id: '11', email: 'contact11@example.com', name: 'Contact Eleven' },
-    { id: '12', email: 'contact12@example.com', name: 'Contact Twelve' },
-    { id: '13', email: 'contact13@example.com', name: 'Contact Thirteen' },
-    { id: '14', email: 'contact14@example.com', name: 'Contact Fourteen' },
-    { id: '15', email: 'contact15@example.com', name: 'Contact Fifteen' },
-    { id: '16', email: 'contact16@example.com', name: 'Contact Sixteen' },
-    { id: '17', email: 'contact17@example.com', name: 'Contact Seventeen' },
-    { id: '18', email: 'contact18@example.com', name: 'Contact Eighteen' },
-    { id: '19', email: 'contact19@example.com', name: 'Contact Nineteen' },
-    { id: '20', email: 'contact20@example.com', name: 'Contact Twenty' }
+    { id: '1',  email: 'contact1@example.com', phone: '55512345678',  name: 'Contact One' },
+    { id: '2',  email: 'contact2@example.com', phone: '55512345678',  name: 'Contact Two' },
+    { id: '3',  email: 'contact3@example.com', phone: '55512345678',  name: 'Contact Three' },
+    { id: '4',  email: 'contact4@example.com', phone: '55512345678',  name: 'Contact Four' },
+    { id: '5',  email: 'contact5@example.com', phone: '55512345678',  name: 'Contact Five' },
+    { id: '6',  email: 'contact6@example.com', phone: '55512345678',  name: 'Contact Six' },
+    { id: '7',  email: 'contact7@example.com', phone: '55512345678',  name: 'Contact Seven' },
+    { id: '8',  email: 'contact8@example.com', phone: '55512345678',  name: 'Contact Eight' },
+    { id: '9',  email: 'contact9@example.com', phone: '55512345678',  name: 'Contact Nine' },
+    { id: '10', email: 'contact10@example.com', phone: '55512345678', name: 'Contact Ten' },
+    { id: '11', email: 'contact11@example.com', phone: '55512345678', name: 'Contact Eleven' },
+    { id: '12', email: 'contact12@example.com', phone: '55512345678', name: 'Contact Twelve' },
+    { id: '13', email: 'contact13@example.com', phone: '55512345678', name: 'Contact Thirteen' },
+    { id: '14', email: 'contact14@example.com', phone: '55512345678', name: 'Contact Fourteen' },
+    { id: '15', email: 'contact15@example.com', phone: '55512345678', name: 'Contact Fifteen' },
+    { id: '16', email: 'contact16@example.com', phone: '55512345678', name: 'Contact Sixteen' },
+    { id: '17', email: 'contact17@example.com', phone: '55512345678', name: 'Contact Seventeen' },
+    { id: '18', email: 'contact18@example.com', phone: '55512345678', name: 'Contact Eighteen' },
+    { id: '19', email: 'contact19@example.com', phone: '55512345678', name: 'Contact Nineteen' },
+    { id: '20', email: 'contact20@example.com', phone: '55512345678', name: 'Contact Twenty' }
   ],
 
   sortedAndFilteredContacts: Ember.computed('contacts.@.name', 'contacts.length', 'filterText', function() {
@@ -81,6 +81,9 @@ export default Ember.Controller.extend({
     },
     show: function(contact) {
       this.transitionToRoute('contacts.contact', contact)
+    },
+    clearFilter: function(){
+      this.set('filterText', null);
     }
   }
   ////////////////////////////////////////
