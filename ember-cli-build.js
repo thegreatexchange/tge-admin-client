ed4e570f325c92eb918f2784bbe83ab17c869b41
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-10-11 21:51:33 -0400

    WIP - deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,35 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+
+  var env = EmberApp.env()|| 'development';
+  var isProductionLikeBuild = ['production'].indexOf(env) > -1;
+  var fingerprintOptions = {
+    enabled: true,
+    extensions: ['js', 'css', 'png', 'jpg', 'gif']
+  };
+  switch (env) {
+    case 'development':
+      fingerprintOptions.prepend = 'http://localhost:4200/';
+      break;
+    case 'production':
+      fingerprintOptions.prepend = 'https://s3.amazonaws.com/tge-assets/admin-client/production/';
+      break;
+  }
+
   var app = new EmberApp(defaults, {
+    fingerprint: fingerprintOptions,
+    emberCLIDeploy: {
+      runOnPostBuild: (env === 'development') ? 'development-postbuild' : false,
+      shouldActivate: true,
+      activate: true
+    },
+    sourcemaps: {
+      enabled: !isProductionLikeBuild,
+    },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS:  { enabled: isProductionLikeBuild },
+
     lessOptions: { paths: [ 'bower_components/bootstrap/less', 'bower_components/jasny-bootstrap/less' ] }
   });
 
