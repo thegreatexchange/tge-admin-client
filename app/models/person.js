b7c24c37b156ff7c0081fbdab2f2803f38ac3688
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-10-19 08:42:49 -0400

    Update person model

--- app/models/person.js
+++ app/models/person.js
@@ -1,12 +1,14 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
 
   name:        attr('string'),
   email:       attr('string'),
   phoneNumber: attr('string'),
-  location:    attr('string')
+  location:    attr('string'),
+  school:      attr('string'),
+  ministry:    attr('string')
 
 });
