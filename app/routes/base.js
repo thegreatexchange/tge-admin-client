5f1d979bb0072bc6468674e3439038c4af6bcf47
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-11-12 22:32:19 -0500

    update to match event client

--- app/routes/base.js
+++ app/routes/base.js
@@ -8,19 +8,44 @@ export default Ember.Route.extend(
   AuthenticatedRouteMixin,
   ////////////////////////////////////////
 {
+  ////////////////////////////////////////
+  // Dependencies
+  ////////////////////////////////////////
+  flashMessages: Ember.inject.service('flashMessages'),
+  ////////////////////////////////////////
 
   ////////////////////////////////////////
   // Properties
   ////////////////////////////////////////
   typeKey:            null,
-  unloadOnDeactivate: true,
+  unloadOnDeactivate: false,
   ////////////////////////////////////////
 
+  ////////////////////////////////////////
+  // Route Lifecycle
+  ////////////////////////////////////////
+  setupController(controller, model) {
+    this._super(controller,model);
+    this.beforeAddObservers(controller, model);
+    controller.addObservers();
+    this.afterAddObservers(controller, model);
+  },
+  beforeAddObservers() {},
+  afterAddObservers()  {},
   deactivate() {
+    this.get('controller').removeObservers();
     this.get('controller').resetProperties();
     if (this.get('typeKey') && this.get('unloadOnDeactivate')) {
       this.store.unloadAll(this.get('typeKey'));
     }
-  }
+  },
+  ////////////////////////////////////////
 
+  ////////////////////////////////////////
+  // Private
+  ////////////////////////////////////////
+  _createRecord(params){
+    return this.store.createRecord(this.get('typeKey'), params);
+  }
+  ////////////////////////////////////////
 });
